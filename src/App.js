9997e04e799d5e3408d343ca4b71fb2459c8d6ad
gayathri1398 <gayathrivasu13@gmail.com>
2021-11-13 23:35:47 +0530

    added the login feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-
+import { useEffect,useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 import Login from './Login';
+import {getTokenFromUrl} from './spotify';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token,setToken] = useState(null);
+
+  useEffect(()=>{
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+
+    if(_token){
+      setToken(_token)
+      spotify.setAccessToken(_token)
+      spotify.getUser().then((user) =>{
+        console.log("heyyy!",user)
+      } )
+    }
+
+
+    console.log("Yoy!!!",token)
+  },[])
+
   return <>
-    <div className="logo">
+    <div className="app">
+     {token? <h1>I am Logged in Yoy!!!</h1>: (
      <Login/>
+     )}
     </div>
   
   </>
--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import logo from "./images/logo.png";
+import { accessUrl } from './spotify';
 
 const Login = () => {
-    return <>(
+    return <>
         <div classname="nav">
         <div className="logo">
          <img src={logo} alt="Spotify"/>
          <h1 className="heading">Spotify</h1>
         </div>
+
         <div className="signup">
             <a href="">sign up</a>
-            <a href="">Login</a>
+            <a href={accessUrl}>Login</a>
         </div>
         </div>
-    )
+    
     </>
 }
 
