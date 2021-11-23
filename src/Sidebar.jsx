51afb8c42a414d17d76f20d542f7b2a5a9e3d2f7
gayathri1398 <gayathrivasu13@gmail.com>
2021-11-23 22:28:52 +0530

    cleared the bug

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SidebarOptions from './SidebarOptions';
-// import Home from '@material-ui/icons/Home';
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from '@mui/icons-material/Search';
 import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
@@ -13,13 +12,16 @@ import './Sidebar.css';
 const Sidebar = () => {
     return<> 
         <div className="sidebar">
-          <img className="sidebar__logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWwSDcPB5BiNYYBFYC9kcG8st464LzORMRsA&usqp=CAU" alt="" />
-        </div>
-        
+          <img className="sidebar__logo"
+           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWwSDcPB5BiNYYBFYC9kcG8st464LzORMRsA&usqp=CAU"
+            alt="spotify" />        
         <SidebarOptions Icon={HomeIcon} title="Home" />
         <SidebarOptions Icon={SearchIcon} title="Search" />
         <SidebarOptions Icon={LibraryAddCheckIcon} title="Library" />
-      
+        
+         <h1 className="sidebar__heading">Playlists</h1>
+         <hr/>
+         </div>  
     </>
 }
 
--- src/SidebarOptions.jsx
+++ src/SidebarOptions.jsx
@@ -5,16 +5,9 @@ import './SidebarOptions.css';
 const SidebarOptions=({Icon,title})=>{
   return <>
   <div className="SidebarOptions">
-    <div className="SidebarOptions__details">
-      
-     { Icon && <Icon className="sidebar__icons"/>}
-      {Icon?<h4>{title}</h4>:<p>{title}</p>}
-    </div>
-      <h1 className="sidebar__heading">Playlists</h1>
-      <hr />
+     {Icon && <Icon className="sidebar__icons"/>}
+     {Icon?<h4>{title}</h4>:<p>{title}</p>}
   </div>
- 
-
   </>
 }
 
