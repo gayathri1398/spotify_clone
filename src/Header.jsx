62439a52c737fbb611c6b2cea3c50b2dea2b7d03
gayathri1398 <gayathrivasu13@gmail.com>
2021-11-30 14:29:57 +0530

    avatar completion

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { Avatar } from '@material-ui/core';
-import './Header.css'
+import {useDataLayerValue} from './DataLayer'
+import './Header.css';
+
 
 const Header =()=>{
+const[{user}, dispatch] = useDataLayerValue();
+console.log({user});
+
 return <>
    <div className="header">
         <div className="header__search">
@@ -14,15 +19,15 @@ return <>
   </div>
     <div className="header__avatar__info">
     <Avatar
-    alt="Remy Sharp"
-    src="/static/images/avatar/1.jpg"
-    style={{
+    alt="gayu"
+    src={user?.images[0].url}
+        style={{
        
         width: "30px",
         height: "30px",
       }} 
     />
-    <h3>Gayathri</h3>
+    <h3>{user?.display_name}</h3>
     </div>
 
    </div>
