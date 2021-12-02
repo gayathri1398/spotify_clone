b56f714b2814ed7f7816d449a0bcb1a6e7e214dc
gayathri1398 <gayathrivasu13@gmail.com>
2021-12-02 16:08:50 +0530

    added the images for the song

--- src/Songrow.jsx
+++ src/Songrow.jsx
@@ -4,7 +4,7 @@ import './Songrow.css'
 const SongRow=({track})=>{
     return <>
     <div className="song_row">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPLtiig4-u_RnWIBStXAmGHgA2lpqIM13K4w&usqp=CAU" 
+        <img src={track.album.images[0].url} 
         alt="song poster" />
       <div className="song_row_details">
           <h5>{track.name}</h5>
